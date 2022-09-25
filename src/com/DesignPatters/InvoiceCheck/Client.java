843e76264e43e92127a1fe462a87ec8869a31fde
RichardDelvin <81515750+RichardDelvin@users.noreply.github.com>
2022-09-25 15:31:37 +0200

    newObserver

--- src/com/DesignPatters/InvoiceCheck/Client.java
+++ src/com/DesignPatters/InvoiceCheck/Client.java
@@ -13,14 +13,14 @@ public class Client {
     public static void main(String[] args) {
 
         InvoiceDownloader realdownloader = new DPTInvoiceDownloader();
-        InvoiceDownloader proxydownloader = new ProxyDownloader(realdownloader);
+        InvoiceDownloader proxyDownloader = new ProxyDownloader(realdownloader);
 
         long startTime = System.currentTimeMillis();
 
 
 
        for(int i = 0; i < 10; i++){
-           String fatura = proxydownloader.downloadInvoice("67B2E3AD6DB1BEA5E9C220B88B4D3422", "2022-09-25T10:50:46 02:00", "L07004002P");
+           String fatura = proxyDownloader.downloadInvoice("67B2E3AD6DB1BEA5E9C220B88B4D3422", "2022-09-25T10:50:46 02:00", "L07004002P");
            System.out.println(fatura);
        }
         long endTime = System.currentTimeMillis();
